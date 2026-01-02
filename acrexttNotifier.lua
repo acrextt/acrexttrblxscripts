909925076956c80cc86b51dd78bf0f5f272db59a
acrextt <ceecodez34@gmail.com>
2026-01-02 16:19:18 +0000

    Update acrexttNotifier.lua

--- acrexttNotifier.lua
+++ acrexttNotifier.lua
@@ -25,7 +25,7 @@ end
 local acrexttNotifier = {}
 
 local function getNotificationsContainer()
-	if NotificationsContainer and NotificationsContainer.Parent then
+	if NotificationsContainer and NotificationsContainer.Parent or PlayerGui:FindFirstChild("AcrexttNotifications") then
 		return NotificationsContainer
 	end
 	
@@ -328,4 +328,5 @@ function acrexttNotifier:notifyError(message, duration)
 end
 
 return acrexttNotifier
+
 
